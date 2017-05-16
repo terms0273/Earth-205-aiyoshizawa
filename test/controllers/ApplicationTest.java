525a306de537336909dd9f5101a17634517dd63f
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-16 18:45:47 +0900

    controllersのテストを追加

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -1,22 +1,20 @@
+package controllers;
+
+
+import .*;
+import Models.User;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
-
 import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
+import play.data.Form;
+import static play.data.Form.form;
+import play.test.FakeApplication;
 
 
 /**
@@ -26,19 +24,25 @@ import static org.fest.assertions.Assertions.*;
 *
 */
 public class ApplicationTest {
-
+    
+    /**
+     * ログイン成功時,メイン画面へ遷移する
+     * ログイン成功時,セッションに値が入っている
+     */
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void testLoginSuccessPage() {
+        FakeApplication app = fakeApplication();
+        
+        Map<String,String> map = new HashMap<>();
+        map.put("Id","admin");
+        map.put("Password","admin");
+        
+        Form<User> form = form(User.class).bind(map);
+        Result result = route(fakeRequest(POST,"/doLogin").withBody(form));
+        
+        assertThat(contentType(result)).isEqualTo("text/html");
+        assertThat(contentAsString(result)).contains("ログアウト");
+        assertThat(session(result)).isNotNull();
     }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
-    }
-
-
+    
 }
