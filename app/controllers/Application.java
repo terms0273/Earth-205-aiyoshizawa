e7605cc52a1fdc74463a6cfadb0fea8067dbbb79
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-18 12:53:41 +0900

    loginとdoLoginメソッドを追加 doLoginは途中

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
-import play.*;
+import models.User;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render());
+    public static Result login() {
+        Form form = new Form(User.class);
+        return ok(login.render(form));
+    }
+    public static Result doLogin(){
+        boolean errFlag = false;
+        Form form = new Form(User.class).bindFromRequest();
+        
+        if(!form.hasErrors()){
+            //dbチェック
+        }else{
+            errFlag = true;
+        }
+        if(!errFlag){
+            return ok(index.render());
+        }else{
+            Form loginForm = new Form(User.class);
+            return ok(login.render(loginForm));
+        }
     }
 
 }
