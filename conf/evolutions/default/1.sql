496b1432306d945679623a27c66ee2f0eb18507e
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-19 18:36:44 +0900

    適当なもの

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,26 +4,22 @@
 # --- !Ups
 
 create table user (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   user_id                   varchar(255),
   password                  varchar(255),
   nick_name                 varchar(255),
-  delete_flag               boolean,
+  delete_flag               tinyint(1) default 0,
   constraint pk_user primary key (id))
 ;
-
-create sequence user_seq;
 
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists user;
+drop table user;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists user_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
